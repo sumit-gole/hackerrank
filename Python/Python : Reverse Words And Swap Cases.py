302cc5577ffb7d00b5fb9792b7b956dd52cb0f2c
Sumit Gole <67162604+sumit-gole@users.noreply.github.com>
2020-07-18 17:53:27 +0530

    Create Python : Reverse Words And Swap Cases.py

---
+++ Python/Python : Reverse Words And Swap Cases.py
@@ -0,0 +1,28 @@
+# COPYRIGHT BY @ SUMIT GOLE - THANK YOU..
+
+#!/bin/python
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the 'reverse_words_order_and_swap_cases' function below.
+# The function is expected to return a STRING.
+# The function accepts STRING sentence as parameter.
+
+def reverse_words_order_and_swap_cases(sentence):
+    words = sentence.split(' ') 
+    sentence = ' '.join(reversed(words)) 
+    return sentence.swapcase()
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    sentence = raw_input()
+
+    result = reverse_words_order_and_swap_cases(sentence)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
